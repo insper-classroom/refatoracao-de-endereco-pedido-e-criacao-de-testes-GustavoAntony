156e13718be3e8803aa539421754cacda5f2c8c7
GustavoAntony <105286051+GustavoAntony@users.noreply.github.com>
2022-09-01 16:29:35 -0300

    bigcommit

---
+++ test/test_integracao1.py
@@ -0,0 +1,10 @@
+import pytest
+import numpy as np
+from classes.Endereco import Endereco
+import http.client as httplib
+from requests.exceptions import ConnectionError
+import requests
+import json
+
+def test_criacao_pessoa_fisica():
+    nome = 'Carlos'
